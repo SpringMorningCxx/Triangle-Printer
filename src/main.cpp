1af5d5f16b6a19e7dbac0e36a66731d26ce8da6e
SpringMorning <springmorning@139.com>
2025-06-23 20:29:41 +0800

    feat(config): add .clang-format configuration
    - Add standardized .clang-format file for consistent code styling
    - Apply clang-format to all source files for uniform formatting
    - Ensure compliance with C++ best practices and style guidelines

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include "triangle_print.hpp"
 
-int main() {
-  TrianglePrint triangle(10);
-  triangle.left_right_angled_triangle();
-  std::println("{:-<30}", "");
-  triangle.inverted_left_right_angled_triangle();
-  std::println("{:-<30}", "");
-  triangle.right_right_angled_triangle();
-  std::println("{:-<30}", "");
-  triangle.inverted_right_right_angled_triangle();
-  std::println("{:-<30}", "");
-  triangle.normal_triangle();
-  std::println("{:-<30}", "");
-  triangle.inverted_normal_triangle();
-  std::println("{:-<30}", "");
-  triangle.diamond();
+int main()
+{
+    TrianglePrint triangle(10);
+    triangle.left_right_angled_triangle();
+    std::println("{:-<30}", "");
+    triangle.inverted_left_right_angled_triangle();
+    std::println("{:-<30}", "");
+    triangle.right_right_angled_triangle();
+    std::println("{:-<30}", "");
+    triangle.inverted_right_right_angled_triangle();
+    std::println("{:-<30}", "");
+    triangle.normal_triangle();
+    std::println("{:-<30}", "");
+    triangle.inverted_normal_triangle();
+    std::println("{:-<30}", "");
+    triangle.diamond();
 }
--- src/triangle_print.cpp
+++ src/triangle_print.cpp
@@ -1,48 +1,55 @@
 #include "triangle_print.hpp"
 
-void TrianglePrint::left_right_angled_triangle() const {
-  for (int i = 1; i <= line_num; i++) {
-    std::println("{}{}",n_blank(line_num - i), n_chars(i,chars));
-  }
+void TrianglePrint::left_right_angled_triangle() const
+{
+    for (int i = 1; i <= line_num; i++) {
+        std::println("{}{}", n_blank(line_num - i), n_chars(i, chars));
+    }
 }
-void TrianglePrint::inverted_left_right_angled_triangle() const {
-  for (int i = 1; i <= line_num; i++) {
-    std::println("{}{}",n_blank(i - 1), n_chars(line_num - i + 1,chars));
-  }
-}
-
-void TrianglePrint::right_right_angled_triangle() const {
-  for (int i = 0; i < line_num; i++) {
-    std::println("{}", n_chars(i + 1,chars));
-  }
+void TrianglePrint::inverted_left_right_angled_triangle() const
+{
+    for (int i = 1; i <= line_num; i++) {
+        std::println("{}{}", n_blank(i - 1), n_chars(line_num - i + 1, chars));
+    }
 }
 
-void TrianglePrint::inverted_right_right_angled_triangle() const {
-  for (int i = 1; i <= line_num; i++) {
-    std::println("{}", n_chars(line_num - i + 1,chars));
-  }
+void TrianglePrint::right_right_angled_triangle() const
+{
+    for (int i = 0; i < line_num; i++) {
+        std::println("{}", n_chars(i + 1, chars));
+    }
 }
 
-void TrianglePrint::normal_triangle() const {
-  const int multi = 2;
-  for (int i = 0; i < line_num; i++) {
-    std::println("{}{}", n_blank(line_num - i - 1),n_chars(i * multi + 1,chars));
-  }
+void TrianglePrint::inverted_right_right_angled_triangle() const
+{
+    for (int i = 1; i <= line_num; i++) {
+        std::println("{}", n_chars(line_num - i + 1, chars));
+    }
 }
 
-void TrianglePrint::inverted_normal_triangle() const {
-  const int multi = 2;
-  for (int i = 0; i < line_num; i++) {
-    std::println("{}{}", n_blank(i), n_chars((line_num - i) * multi - 1,chars));
-  }
+void TrianglePrint::normal_triangle() const
+{
+    const int multi = 2;
+    for (int i = 0; i < line_num; i++) {
+        std::println("{}{}", n_blank(line_num - i - 1), n_chars(i * multi + 1, chars));
+    }
 }
 
-void TrianglePrint::diamond() const {
-  int multi = 2;
-  for (int i = 0; i < line_num; i++) {
-    std::println("{}{}", n_blank(line_num - i - 1), n_chars(i * multi + 1,chars));
-  }
-  for (int i = 1; i < line_num; i++) {
-    std::println("{}{}", n_blank(i), n_chars((line_num - i) * multi - 1,chars));
-  }
+void TrianglePrint::inverted_normal_triangle() const
+{
+    const int multi = 2;
+    for (int i = 0; i < line_num; i++) {
+        std::println("{}{}", n_blank(i), n_chars((line_num - i) * multi - 1, chars));
+    }
+}
+
+void TrianglePrint::diamond() const
+{
+    int multi = 2;
+    for (int i = 0; i < line_num; i++) {
+        std::println("{}{}", n_blank(line_num - i - 1), n_chars(i * multi + 1, chars));
+    }
+    for (int i = 1; i < line_num; i++) {
+        std::println("{}{}", n_blank(i), n_chars((line_num - i) * multi - 1, chars));
+    }
 }
